634e49b945a283e5746f93823b15183032408c6b
MateusKen <54600427+MateusKen@users.noreply.github.com>
2023-02-09 10:36:47 -0300

    Update exercicio5.c

--- 01-funcoes_e_vetores/exercicio5.c
+++ 01-funcoes_e_vetores/exercicio5.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 //TIA: 32216289 NOME: Mateus Kenzo Iochimoto TURMA: 3G
 
+/* 5. Escreva uma função que recebe o vetor v[], e um inteiro n, que indica o tamanho do vetor.
+Devolva a soma dos elementos pares. Por exemplo, para v = [1, 2, 3, 4, 5], o resultado é 6. */
+
 int main()
 {
   int i, soma = 0;
