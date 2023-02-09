7130259f97c634b5c1598d66a2952aace2667aa1
MateusKen <54600427+MateusKen@users.noreply.github.com>
2023-02-09 10:34:07 -0300

    Update exercicio2.c

--- 01-funcoes_e_vetores/exercicio2.c
+++ 01-funcoes_e_vetores/exercicio2.c
@@ -2,6 +2,10 @@
 #include <stdbool.h>
 //TIA: 32216289 NOME: Mateus Kenzo Iochimoto TURMA: 3G
 
+/*2. Escreva uma função chamado ultimoDigito() que receba dois inteiros a e b, não negativos
+e devolve true se a e b tem os mesmos últimos dígitos. Por exemplo, 57 e 27, ambos têm
+os últimos dígitos iguais a 7. Utilize a biblioteca stdbool.h para devolver booleano.*/
+
 int ultimoDigito(int a, int b)
 {
   int lda, ldb;
