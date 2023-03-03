36e6c469f124e7c7a4f7df2fb72ba64b507aa755
MateusKen <54600427+MateusKen@users.noreply.github.com>
2023-03-03 11:26:37 -0300

    Update exercicio4.c

--- 02_exercicios_de_string/exercicio4.c
+++ 02_exercicios_de_string/exercicio4.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+// NOME: Mateus Kenzo Iochimoto TIA: 32216289
+
+/*4. Escreva uma função que recebe um vetor de caractere e um caracter, a função remove
+todas as ocorrências do caracter na string sem deixar buracos.*/
+
 void removeChar(char palavra[], char character[]){
     int i, j;
     int len = strlen(palavra);
