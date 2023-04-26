127909f1fa58d4639d7fbeb3728b29839e589667
MateusKen <54600427+MateusKen@users.noreply.github.com>
2023-04-26 08:32:34 -0300

    Update and rename exercicio4 (2).c to exercicio4 .c

--- 10 - recursividade/exercicio4 .c
+++ 10 - recursividade/exercicio4 .c
@@ -1,5 +1,13 @@
 //NOME: Mateus Kenzo Iochimoto
 //TIA: 32216289
+/*
+4. A potência de um número (a^n) pode ser definida recursivamente
+a ^n=
+1, se n = 0
+a · a^(n−1) , se n > 0
+Escreva a função recursiva que recebe um número real a e um número natural n e, sem usar
+funções da biblioteca matemática, devolve a^n.
+*/
 #include <stdio.h>
 
 int an(int a, int n)
