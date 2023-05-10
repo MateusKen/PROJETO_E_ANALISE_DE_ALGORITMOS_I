91f50f2cfbdd7653366730566961120b82584472
MateusKen <54600427+MateusKen@users.noreply.github.com>
2023-05-10 09:46:15 -0300

    Update main.c

--- Trabalho dicionário Samuel/main.c
+++ Trabalho dicionário Samuel/main.c
@@ -1,23 +1,37 @@
 #include <stdio.h>
 
-int main(){
+int buscaBinaria()
+{
+  
+}
+
+int insercaoRecursiva()
+{
+  
+}
+
+int main()
+{
   FILE *arquivo;
   arquivo = fopen("texto.txt", "r");
-  char buffer[256];
+  char buffer[256]; // cria string que vai receber cada linha do texto
   
   if (arquivo == NULL) // caso o arquivo referenciado não exista
     printf("Não foi possível encontrar o arquivo");
   else
   {
-    while(strcmp(fgets(buffer, 256, arquivo), ".") != 0){
-      char * token = strtok(buffer, " ");
-      // loop through the string to extract all other tokens
-      while( token != NULL ) {
-        printf( "%s\n", token ); //printing each token
+    while(strcmp(fgets(buffer, 256, arquivo), ".") != 0) // lê as linhas do arquivo de texto e para quando achar "."
+    { 
+      buffer[strlen(buffer)-1] = 0; // tira o \n
+      char * token = strtok(buffer, " "); // faz o split() da string por " "
+      while( token != NULL ) 
+      {
+        printf( "%s\n", token ); //printa cada palavra
       
-        token = strtok(NULL, " ");
+        token = strtok(NULL, " "); // vai para próxima palavra
+      }
     }
-  }
 
   fclose(arquivo);
-}}
+  }
+}
