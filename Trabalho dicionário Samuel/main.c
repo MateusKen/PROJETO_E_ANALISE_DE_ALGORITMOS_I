30d2764a136e6efe1c84e1eaf04133abc731a598
MateusKen <54600427+MateusKen@users.noreply.github.com>
2023-05-10 10:20:34 -0300

    Update main.c

--- Trabalho dicionário Samuel/main.c
+++ Trabalho dicionário Samuel/main.c
@@ -15,6 +15,8 @@ int main()
   FILE *arquivo;
   arquivo = fopen("texto.txt", "r");
   char buffer[256]; // cria string que vai receber cada linha do texto
+  char **palavras;
+  palavras = malloc(sizeof(char*)*100); // alocando um vetor de char com 100 posições
   
   if (arquivo == NULL) // caso o arquivo referenciado não exista
     printf("Não foi possível encontrar o arquivo");
@@ -26,6 +28,10 @@ int main()
       char * token = strtok(buffer, " "); // faz o split() da string por " "
       while( token != NULL ) 
       {
+        /*
+        alocação dinamica da palavra no vetor
+        palavras[indice]=malloc(sizeof(char)*strlen(token));
+        */
         printf( "%s\n", token ); //printa cada palavra
       
         token = strtok(NULL, " "); // vai para próxima palavra
